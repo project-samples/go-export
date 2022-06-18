aaa7281f6d6d99e884af604aa8d70e99713634da
Duc Nguyen <minhduc140583@gmail.com>
2022-06-18 16:40:32 +0700

    Format code

--- internal/app/app.go
+++ internal/app/app.go
@@ -40,8 +40,8 @@ func NewApp(ctx context.Context, conf Config) (*ApplicationContext, error) {
 type User struct {
 	Id          string     `json:"id" gorm:"column:id;primary_key" bson:"_id" format:"%011s" length:"11" dynamodbav:"id" firestore:"id" validate:"required,max=40"`
 	Username    string     `json:"username" gorm:"column:username" bson:"username" length:"10" dynamodbav:"username" firestore:"username" validate:"required,username,max=100"`
-	Email       *string    `json:"email" gorm:"column:email" bson:"email" dynamodbav:"email" firestore:"email"  length:"31" validate:"email,max=100"`
-	Phone       string     `json:"phone" gorm:"column:phone" bson:"phone" dynamodbav:"phone" firestore:"phone"  length:"20" validate:"required,phone,max=18"`
+	Email       *string     `json:"email" gorm:"column:email" bson:"email" dynamodbav:"email" firestore:"email" length:"31" validate:"email,max=100"`
+	Phone       string     `json:"phone" gorm:"column:phone" bson:"phone" dynamodbav:"phone" firestore:"phone" length:"20" validate:"required,phone,max=18"`
 	Status      bool       `json:"status" gorm:"column:status" true:"1" false:"0" bson:"status" dynamodbav:"status" format:"%5s" length:"5" firestore:"status" validate:"required"`
 	CreatedDate *time.Time `json:"createdDate" gorm:"column:createdDate" bson:"createdDate" length:"10" format:"dateFormat:2006-01-02" dynamodbav:"createdDate" firestore:"createdDate" validate:"required"`
 }
